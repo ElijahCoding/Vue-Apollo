0eac9526c77db97747c7d8cd82bcf07832e2c8e2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-02 13:30:21 +0300

    addPost mutation

--- resolvers.js
+++ resolvers.js
@@ -4,6 +4,21 @@ module.exports = {
     },
 
     Mutation: {
+        addPost: async (
+            root,
+            { title, imageUrl, categories, description, creatorId }
+        ) => {
+            const newPost = await new Post({
+                title,
+                imageUrl,
+                categories,
+                description,
+                createdBy: creatorId
+            }).save();
+
+            return newPost;
+        },
+
         signupUser: async (root, { username, email, password }, { User }) => {
             const user = await User.findOne({ username });
 
