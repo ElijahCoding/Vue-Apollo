4ea4471824cfe6626aed49127498d5e13e6d37e6
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-02 23:42:49 +0300

    showing posts

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <v-container>
     <h1>Home</h1>
+    <ul v-for="post in getPosts" :key="post._id">
+      <li>
+        {{post.title}} {{post.imageUrl}} {{post.description}}
+      </li>
+      <li>{{post.likes}}</li>
+    </ul>
   </v-container>
 </template>
 
@@ -19,6 +25,7 @@
                             title
                             imageUrl
                             description
+                            likes
                         }
                     }
                 `
