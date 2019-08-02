c49f1b71eb4434bb665e30a1cbdefc2cc6db5c56
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-02 13:23:31 +0300

    sign up user

--- resolvers.js
+++ resolvers.js
@@ -4,6 +4,20 @@ module.exports = {
     },
 
     Mutation: {
+        signupUser: async (root, { username, email, password }, { User }) => {
+            const user = await User.findOne({ username });
 
+            if (user) {
+                throw new Error("User already exists");
+            }
+
+            const newUser = await new User({
+                username,
+                email,
+                password
+            }).save();
+
+            return newUser;
+        }
     }
 }
