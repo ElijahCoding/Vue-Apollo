35d64d0f9cdd50313ce2572469b9e0065a708f29
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-08 11:19:43 +0300

    built

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -12,7 +12,7 @@
 
     <v-flex xs12>
       <v-carousel v-if="!loading && posts.length > 0" v-bind="{ 'cycle': true }" interval="3000">
-        <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
+        <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl" @click.native="goToPost(post._id)">
           <h1 id="carousel__title">{{post.title}}</h1>
         </v-carousel-item>
       </v-carousel>
@@ -21,37 +21,38 @@
 </template>
 
 <script>
-    import { mapGetters } from "vuex";
+import { mapGetters } from "vuex";
 
-    export default {
-        name: "home",
-
-        created () {
-            this.handleGetCarouselPosts()
-        },
-
-        computed: {
-            ...mapGetters(["loading", "posts"])
-        },
-
-        methods: {
-            handleGetCarouselPosts () {
-                this.$store.dispatch('getPosts')
-            }
-        }
-    };
+export default {
+  name: "home",
+  created() {
+    this.handleGetCarouselPosts();
+  },
+  computed: {
+    ...mapGetters(["loading", "posts"])
+  },
+  methods: {
+    handleGetCarouselPosts() {
+      // reach out to Vuex store, fire action that gets posts for carousel
+      this.$store.dispatch("getPosts");
+    },
+    goToPost(postId) {
+      this.$router.push(`/posts/${postId}`);
+    }
+  }
+};
 </script>
 
 <style>
-    #carousel__title {
-        position: absolute;
-        background-color: rgba(0, 0, 0, 0.5);
-        color: white;
-        border-radius: 5px 5px 0 0;
-        padding: 0.5em;
-        margin: 0 auto;
-        bottom: 50px;
-        left: 0;
-        right: 0;
-    }
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
 </style>
